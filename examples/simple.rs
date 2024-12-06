593fbb1f4e3c8e1b986b2752e402e9abb45105e1
rojer-98 <rojermagnuson98@gmail.com>
2024-12-06 10:12:14 +0300

    [ref] Add examples

---
+++ examples/simple.rs
@@ -0,0 +1,36 @@
+use anyhow::Result;
+use tracing::{debug, info, trace};
+
+use unconfig::{config, configurable, logger};
+
+#[configurable("${CONFIG}")]
+struct Access {
+    url: String,
+    access_code: String,
+}
+
+#[configurable("config_2.yml")]
+struct User {
+    name: String,
+    pass: String,
+}
+
+#[configurable("config_2.yml")]
+struct CustomUser {
+    name: String,
+    pass: String,
+    custom_var: i32,
+}
+
+#[logger]
+#[config(User, Access, CustomUser)]
+fn main() -> Result<()> {
+    info!("Hello world!");
+    trace!("Hello world!");
+    debug!("Hello world!");
+
+    println!("{:?}", CONFIG_USER.name());
+    println!("{:?}", CONFIG_CUSTOM_USER.custom_var());
+
+    Ok(())
+}
